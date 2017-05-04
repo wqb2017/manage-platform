e15f020fed5daa4a0ed65960e6999a8178d563fe
wangqibiao <wqb2017@yeah.net>
2017-05-04 14:42:43 +0800

    vue2+element+webpack分享

--- src/components/page/Datasurver.vue
+++ src/components/page/Datasurver.vue
@@ -3,34 +3,21 @@
         <div class="button-right">
             <el-button type="primary" @click="editManage">新 增</el-button>
         </div>
-        <el-table
-            :data="tableData"
-            style="width: 100%">
-            <el-table-column
-                prop="date"
-                label="日期"
-                width="180">
-            </el-table-column>
-            <el-table-column
-                prop="name"
-                label="姓名"
-                width="180">
-            </el-table-column>
-            <el-table-column
-                prop="address"
-                label="地址">
-            </el-table-column>
+        <el-table :data="tableData" style="width: 100%">
+            <el-table-column prop="date" label="日期" width="180"></el-table-column>
+            <el-table-column prop="name"  label="姓名"  width="180"></el-table-column>
+            <el-table-column prop="address"  label="地址"> </el-table-column>
         </el-table>
+        <div class="block">
+            <el-pagination layout="prev, pager, next" :total="50"></el-pagination>
+        </div>
         <el-dialog v-model="dialogTableVisible">
             <el-form :model="dynamicValidateForm" ref="dynamicValidateForm" label-width="100px" class="demo-dynamic">
                 <el-form-item
                     prop="email"
                     label="邮箱"
-                    :rules="[
-      { required: true, message: '请输入邮箱地址', trigger: 'blur' },
-      { type: 'email', message: '请输入正确的邮箱地址', trigger: 'blur,change' }
-    ]"
-                >
+                    :rules="[{ required: true, message: '请输入邮箱地址', trigger: 'blur' },
+                            { type: 'email', message: '请输入正确的邮箱地址', trigger: 'blur,change' }]">
                     <el-input v-model="dynamicValidateForm.email"></el-input>
                 </el-form-item>
                 <el-form-item
@@ -38,10 +25,7 @@
                     :label="'域名' + index"
                     :key="domain.key"
                     :prop="'domains.' + index + '.value'"
-                    :rules="{
-      required: true, message: '域名不能为空', trigger: 'blur'
-    }"
-                >
+                    :rules="{ required: true, message: '域名不能为空', trigger: 'blur' }" >
                     <el-input v-model="domain.value"></el-input></el-form-item>
                 <el-form-item>
                     <el-button type="primary" @click="submitForm('dynamicValidateForm')">提交</el-button>
@@ -50,9 +34,11 @@
                 </el-form-item>
             </el-form>
         </el-dialog>
+        <user-info></user-info>
     </div>
 </template>
 <script>
+    import UserInfo from "./UserInfo.vue";
     export default {
         data() {
             return {
@@ -105,6 +91,9 @@
             editManage: function () {
                 this.dialogTableVisible = true;
             }
+        },
+        components:{
+            'user-info':UserInfo
         }
     }
 </script>
